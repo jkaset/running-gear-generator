c1f0de437414e9eb252fc098bca06d9633878ae8
Jasmin Kaset <kasetski@gmail.com>
2020-11-01 17:43:00 -0600

    tried more WeatherList.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 import { TempFormComponent } from "./temperature/TempForm.js"
-TempFormComponent()
 import { getWeather, useWeather } from "./weather/WeatherProvider.js"
 import { ZipFormComponent } from "./weather/Weather.js"
 
 
 import "./temperature/TempForm.js"
 import "./temperature/TempList.js"
+import {dispatchWeather} from "./weather/WeatherList.js"
 
+TempFormComponent()
 getWeather(37210)
-useWeather
-
+useWeather()
 ZipFormComponent()
+dispatchWeather()
--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -2,37 +2,33 @@ import { getWeather, useWeather }  from "./WeatherProvider.js"
 
 const eventHub = document.querySelector("#container")
 
-eventHub.addEventListener("zipSubmitted", event => {
-  let zips = []
-  const zip = event.detail.zipEntered
-  console.log(zip)
-  getWeather(zip)
+// eventHub.addEventListener("zipSubmitted", event => {
+//   let zips = []
+//   const zip = event.detail.zipEntered
+//   console.log(zip)
+//   getWeather(zip)
   
-  .then(() => 
-    zips = useWeather())
-  
-  
-})
-
-//make html card for render weather
-// const weatherCard = (weather) => {
-//   return `
-    
-//       <p>${weather.name}</p>
-    
-//   `
-// }
-
-// export const weatherList = () => {
-  
-//   getWeather().then( 
-//     () => {
-//       const weatherContainer = document.querySelector(".renderGear")
-//       const weatherArray = useWeather() 
-//       let buildWeatherList = ""
-//       for (const weather of weatherArray) {
-//         buildWeatherList += weatherCard(weather)
-//     }
-//   weatherContainer.innerHTML += `${buildWeatherList}`
+//   .then(() => 
+//     zips = useWeather())
 // })
-// }
+
+export const dispatchWeather = () => {
+  
+
+  eventHub.addEventListener("zipSubmitted", e => {
+    const zip = e.detail.zipEntered
+    //console.log(zip)
+    
+    getWeather(zip)
+    .then(() => {
+      const contentTarget = document.querySelector(".renderGear")
+      let weatherHTMLString = ""
+      const weatherArray = useWeather()
+      console.log(weatherArray)
+      
+      console.log(weatherHTMLString)
+      contentTarget.innerHTML = `${weatherHTMLString}`
+    })
+  })
+}
+    
