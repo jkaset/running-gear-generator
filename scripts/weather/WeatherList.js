f9aed78f76a4c979fd9a2b0054442b988fe7b9df
Jasmin Kaset <kasetski@gmail.com>
2020-11-01 16:24:08 -0600

    playing with weather

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -3,13 +3,29 @@ import { getWeather, useWeather }  from "./WeatherProvider.js"
 const eventHub = document.querySelector("#container")
 
 eventHub.addEventListener("zipSubmitted", event => {
-  let zips = []
-  const zip = event.detail.zipEntered
-  console.log(zip)
-  getWeather(zip)
   
-  .then(() => 
-    zips = useWeather())
-  
+  getWeather().then( 
+    () => {
+      const weatherContainer = document.querySelector(".renderGear")
+      const weatherArray = useWeather() 
+      let buildWeatherList = ""
+      for (const weatherObj of weatherArray) {
+        buildWeatherList += weatherRenderCard(weatherObj)
+    }
+  weatherContainer.innerHTML += `${buildWeatherList}`
+})
   
 })
+
+
+//event listener started with group
+// eventHub.addEventListener("zipSubmitted", event => {
+// let zips = []
+//   const zip = event.detail.zipEntered
+//   console.log(zip)
+//   getWeather(zip)
+  
+//   .then(() => 
+//     zips = useWeather())
+
+//   })
