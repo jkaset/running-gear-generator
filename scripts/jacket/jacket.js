6106e12e7a42a4b31c66151b6d39e81506150c15
Kristen Steele <ksteelephoto@gmail.com>
2020-11-01 21:55:29 -0600

    add eventlistener to listen for zip submit, use zip to retrieve weather info and call jacket render function. add jacket render function to render a statement based on temperature for given zip.

--- scripts/jacket/jacket.js
+++ scripts/jacket/jacket.js
@@ -2,3 +2,22 @@ import { getWeather } from "../weather/WeatherProvider.js"
 const eventHub = document.querySelector("#container")
 const contentTarget = document.querySelector(".renderGear")
 
+eventHub.addEventListener("zipSubmitted", event => {
+    // console.log("I hear the zip was submitted!");
+    const zipToUse = event.detail.zipEntered
+    getWeather(zipToUse)
+      .then(returnedWeather => {
+          const weatherForUse = returnedWeather
+          jacketRender(weatherForUse)
+      })
+          // console.log("Here's the weather you asked for:", weather)
+        })
+
+const jacketRender = weatherObj => {
+        if (weatherObj.main.feels_like >= 65) {
+            contentTarget.innerHTML = `<h3>Leave your jacket at home today.</h3>`
+        } else {
+            contentTarget.innerHTML = `<h3>You will need a jacket today.</h3>`
+        }
+    
+}
