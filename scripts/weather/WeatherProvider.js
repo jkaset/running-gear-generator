e6310d40f1620da396fcb9c5a11f34cd2424d3e8
Kristen Steele <ksteelephoto@gmail.com>
2020-11-01 20:41:20 -0600

    rework getWeather to return weather object in array

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,15 +1,19 @@
 import keys from "../../Settings.js"
 
-let weather = []
+// let weather = {}
 
 //renders form to the dom
 export const getWeather = (zipcode) => {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${keys.weatherKey}`)
   .then(response => response.json())
   .then(parsedResponse => {
-    console.log(parsedResponse)
-    weather = parsedResponse
+    const weather = parsedResponse
+    return weather
   })
 }
+
+// export const useWeather = () => {
+//   return weather.slice()
+// }
 
 //json-server -p 8088 -w notes.json
